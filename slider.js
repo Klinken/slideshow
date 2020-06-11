250c71ff78ab0c753e0d30d166c12a0a242db704
SørenKristianKlinkwo <SrenKristianKlinkwo@LAPTOP-QLMQ5RUS>
2020-06-11 14:13:02 +0200

    Formatted javascript and added animation timing function.

--- slider.js
+++ slider.js
@@ -1,20 +1,17 @@
-
 //This keeps track of the position, when changing slides in the slider, starts at first image
 var currentPosition = 0;
 
-var timeBetweenSlides = 3000; //This is set in milliseconds
-var animationTime = 1;
+var timeBetweenSlides = 5000; //This is set in milliseconds
+var animationTime = 1; //This is set in seconds
 
 var automaticSliderOn = true;
 
 //This counts the amount of images in the slider
 var images = document.getElementById("slides").children;
 
-console.log(images);
-
 //This adds ID to images within the slides
-for(var i = 0; i < images.length; i++){
-    images[i].id = `image-${i}`;
+for (var i = 0; i < images.length; i++) {
+    images[i].id = "image-" + i;
     images[i].className = "slide-image";
 }
 
@@ -22,17 +19,17 @@ for(var i = 0; i < images.length; i++){
 styleSlide(currentPosition);
 
 //This function changes the current position by 1, if given the word "add", it goes forward, else it goes backwards
-function changePosition(addOrSubstract){
+function changePosition(addOrSubstract) {
 
     images[currentPosition].style.display = "";
 
     addOrSubstract == "add" ? currentPosition++ : currentPosition--;
 
-    if(currentPosition >= images.length){
+    if (currentPosition >= images.length) {
         currentPosition = 0;
         styleSlide(currentPosition);
 
-    } else if(currentPosition < 0){
+    } else if (currentPosition < 0) {
         currentPosition = images.length - 1;
         styleSlide(currentPosition);
 
@@ -43,42 +40,49 @@ function changePosition(addOrSubstract){
 }
 
 //Helper function, used for styling the current slide
-function styleSlide(currentPosition){
-    images[currentPosition].style.display = "block";
-    images[currentPosition].style.animation = "slideanimation 1s";
-    document.getElementById("slide-text").innerHTML = "<p>" + images[currentPosition].alt + "</p>";
+function styleSlide(position) {
+    images[position].style.display = "block";
+    images[position].style.animation = "slideanimation " + animationTime + "s";
 
+    document.getElementById("slide-text").innerHTML = "<p>" + images[position].alt + "</p>";
+
+    if (automaticSliderOn) {
+        images[currentPosition].title = "Click to stop the slideshow";
+    }
 }
 
+
 //The following automatically changes the images after a given time
-var automaticSlider = setInterval(function(){changePosition("add")}, timeBetweenSlides);
-
-
-//Visual cues for the play/pause functions
-
-
+var automaticSlider = setInterval(function () {
+    changePosition("add");
+}, timeBetweenSlides);
 
 
 /* Sets the onclick event on the buttons used to control the slide */
 
-document.getElementById("button-forward").addEventListener("click", function(){
+document.getElementById("button-forward").addEventListener("click", function () {
     changePosition("add");
     clearInterval(automaticSlider);
 });
 
-document.getElementById("button-backward").addEventListener("click", function(){
+document.getElementById("button-backward").addEventListener("click", function () {
     changePosition("substract");
     clearInterval(automaticSlider);
-})
+});
 
 document.getElementById("slides").addEventListener("click", function () {
-    
-    if(automaticSliderOn){
-        clearInterval(automaticSlider);
+
+    if (automaticSliderOn) {
         automaticSliderOn = false;
+        clearInterval(automaticSlider);
+        images[currentPosition].title = "Click to start the slideshow";
+
     } else {
-        automaticSlider = setInterval(function(){changePosition("add")}, timeBetweenSlides);
+        automaticSlider = setInterval(function () {
+            changePosition("add");
+        }, timeBetweenSlides);
         automaticSliderOn = true;
+        images[currentPosition].title = "Click to stop the slideshow";
     }
-    
+
 });
